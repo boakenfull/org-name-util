cf3e51d40aa92989745c8b014af46f4d0fc40103
Ben Oakenfull <benoakenfull@gmail.com>
2021-04-10 18:11:05 +0100

    update

--- src/org-name-util.ts
+++ src/org-name-util.ts
@@ -1,6 +1,5 @@
 // Anything exported from this file is importable by other in-browser modules.
 export async function publicApiFunction() {
     const resp = await fetch('https://restcountries.eu/rest/v2/all')
-
     return resp.json();
 }
