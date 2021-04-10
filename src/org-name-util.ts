066f0fce548efba4e2a658480582a7559134efb3
Ben Oakenfull <benoakenfull@gmail.com>
2021-04-10 18:05:47 +0100

    initial commit

---
+++ src/org-name-util.ts
@@ -0,0 +1,6 @@
+// Anything exported from this file is importable by other in-browser modules.
+export async function publicApiFunction() {
+    const resp = await fetch('https://restcountries.eu/rest/v2/all')
+
+    return resp.json();
+}
